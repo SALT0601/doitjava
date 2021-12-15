310300d56714e5122d9404bed60dc42643d02df4
SALT0601 <thfxm0601@gmail.com>
2021-12-16 02:25:15 +0900

    chap7-array

--- Chpa7/src/array/ArrayTest.java
+++ Chpa7/src/array/ArrayTest.java
@@ -3,7 +3,16 @@ package array;
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		ssfgrd
+		int[] numbers = new int[3] ;
+		numbers[0]=1;
+		numbers[1]=2;
+		numbers[2]=3;
+		
+		for(int i = 0; i<numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+		System.out.println(numbers.length);
 	}
 
 }
